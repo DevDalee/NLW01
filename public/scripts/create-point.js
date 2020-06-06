a7443c214c3a53c88514a666abab2a4515801980
Marcos Dalessandro <mdclima12@gmail.com>
2020-06-05 23:13:30 -0300

    :coffee: Trying solve the bug

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -1,53 +1,51 @@
-    function populateUFs(){
-        const ufSelect = document.querySelector('select[name=uf]')
+function populateUFs(){
+    const ufSelect = document.querySelector("select[name=uf]")
 
-        fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-        .then( res => res.json())
-        .then( states => {
-            
-            for ( const state of states){
-                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-            }
-        })
-    }
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( res => res.json())
+    .then( states => {
+        
+        for ( const state of states ) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
 
-    populateUFs()
+    })
+}
 
-
-    function getCities(event) {
-        const citySelect = document.querySelector("select[name=city]")
-        const stateInput = document.querySelector("[name=state]")
-
-        const ufValue = event.target.value
-
-        const indexOfSelectedState = event.target.selectedIndex
-        stateinput = event.target.options[indexOfSelectedState].text
-
-        const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+populateUFs()
 
 
-        citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
-        citySelect.disabled = true
+function getCities(event) {
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
 
-        fetch(url)
-        .then( res => res.json())
-        .then( cities => {
-            
+    const ufValue = event.target.value
 
-            for ( const city of cities){
-                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
-            }
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
 
-            citySelect.disabled = false
-        })
-    }
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
 
+    fetch(url)
+    .then( res => res.json() )
+    .then( cities => {
+        
+
+        for ( const city of cities){
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+
+        citySelect.disabled = false
+    })
+}
 
 
 document
-    .querySelector('select[name=uf]')
+    .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
 
 //itens de coleta
@@ -55,7 +53,7 @@ document
 const itemsToCollect = document.querySelectorAll(".items-grid li")
 
 for (const item of itemsToCollect){
-    item.addEventListener("click", handleSelectedItem)
+item.addEventListener("click", handleSelectedItem)
 }
 const collectedItems = document.querySelector("input[name=items]")
 
@@ -63,37 +61,37 @@ const collectedItems = document.querySelector("input[name=items]")
 let selectedItems = []
 
 function handleSelectedItem(event){
-    const itemLi= event.target
+const itemLi= event.target
 
-    // add or remove uma classe com JS
-    itemLi.classList.toggle("selected")
+// add or remove uma classe com JS
+itemLi.classList.toggle("selected")
 
-    const itemId= itemLi.dataset.id
+const itemId= itemLi.dataset.id
 
-    //verificar se existem itens selecionados, se sim pegar os itens selecionados
+//verificar se existem itens selecionados, se sim pegar os itens selecionados
 
-    const alredySelected = selectedItems.findIndex( item => {
-        const itemFound = item  == itemId //true or false
-        return itemFound
+const alredySelected = selectedItems.findIndex( item => {
+    const itemFound = item  == itemId //true or false
+    return itemFound
+})
+
+// se já estiver selecionado, tirar da seleção
+if ( alredySelected >= 0) {
+    //tirar da seleção
+    const filteredItems = selectedItems.filter( item => {
+        const itemIsDifferent = item != itemId //false
+        return itemIsDifferent
     })
 
-    // se já estiver selecionado, tirar da seleção
-    if ( alredySelected >= 0) {
-        //tirar da seleção
-        const filteredItems = selectedItems.filter( item => {
-            const itemIsDifferent = item != itemId //false
-            return itemIsDifferent
-        })
+    selectedItems = filteredItems
+} else {
+    //se não estiver selecionado adcionar à seleção
 
-        selectedItems = filteredItems
-    } else {
-        //se não estiver selecionado adcionar à seleção
+    selectedItems.push(itemId)
 
-        selectedItems.push(itemId)
+}
 
-    }
-    
-    // atualizar o campo escondido com os itens selecionados
-    collectedItems.value = selectedItems
-    
+// atualizar o campo escondido com os itens selecionados
+collectedItems.value = selectedItems
+
 }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const server = express()
 
+//pegar o banco de dados
+const db = require("./database/db")
+
 //configurar pasta publica
 server.use(express.static("public"))
 
@@ -17,14 +20,34 @@ nunjucks.configure("src/views", {
 //página inicial
 //req: Requsição
 //res: Resposta
+
+
 server.get("/", (req,res)=>{
     return res.render("index.html", { title: "" })
 })
+
+
 server.get("/create-point", (req,res) =>{
+
+    //req.query: Query Stings da nossa url
+    console.log(req.query)
+
     return res.render("create-point.html")
 })
+
+
 server.get("/search", (req, res) =>{
-    return res.render("search-results.html")
+
+    //pegar os dados do banco de dados//    
+    db.all(`SELECT * FROM places`, function(err, rows){
+       if(err) {
+           return console.log(err)
+      }
+      const total = rows.length 
+      //Mostrar a página html com os dados do banco de dados
+      return res.render("search-results.html", { places: rows, total })
+     })
+
 })
 
 //ligar o servidor
